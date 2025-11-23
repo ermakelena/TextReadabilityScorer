b2fc7f6390ed83e82f3cea2c342aa8b2c6765efa
elena <eiermal@edu.hse.ru>
2025-11-23 13:00:57 +0300

    added structure

---
+++ src/scorer/scorer.py
@@ -0,0 +1,14 @@
+class TextDocument:
+    """Loads and stores text for analysis from .txt document"""
+
+class ReadabilityMetrics:
+    """Contains all calculated readability metrics"""
+
+class ReadabilityAnalyzer:
+    """Main class for text analysis and metrics calculation"""
+
+class TextSimplifier:
+    """Generates text improvement recommendations"""
+
+class ReadabilityReport:
+    """Creates final report in .json format"""
